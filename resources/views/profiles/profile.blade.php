062d2e4d337d77b69d80216a3a9900da8bc801fb
salman zafar <salmanzafar949@gmail.com>
2017-08-08 01:03:21 -0700

    this is my first Commit

---
+++ resources/views/profiles/profile.blade.php
@@ -0,0 +1,45 @@
+ @extends('layouts.app')
+
+
+
+ @section('content')
+              
+              <div class="container">
+
+                <div class="col-lg-4">
+
+                     <div class="panel-default"> 
+                      
+                      <div class="panel-heading">
+                      <p class="text-center">
+                           {{ $user -> name}}'s Profile.
+
+                        </p>
+                       </div>
+
+                        <div class="panel-body">
+                        <center>
+                         <img src="{{ Storage::url($user->avatar)}}" width="140px" height="140px" style="border-radius:50%;" alt="">
+                        </center>
+
+                          <p class="text-center">
+                             @if( Auth::id() == $user -> id)
+                             <a href="{{route('profile.edit')}}" class="btn btn-lg btn-info">Edit your Profile</a>
+                             @endif
+                          </p>
+                                
+                        </div>
+                    
+                     </div>
+                    
+                    <center>
+                    
+                    <p class="text-primary">
+                     {{$user->email}}
+                     </p>   
+                     </center>  
+                </div>
+                  <img src="/storage/defaults/avatars/female.png" class="img-responsive" alt="" width="250px">
+              </div>
+
+ @stop
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/profile/{slug}', [
+
+        'uses' => 'ProfilesController@index',
+        'as' => 'profile'
+    ]);
+Route::group(['middleware'=>'auth'], function(){
+
+    
+    // Route::get('/profile/{slug}', [
+
+    //     'uses' => 'ProfilesController@index',
+    //     'as' => 'profile'
+    // ]);
+
+    Route::get('/profile/edit/profile', [
+
+        'uses' => 'ProfilesController@edit',
+        'as' => 'profile.edit',
+    ]);
+
+     Route::post('/profile/update/update', [
+
+        'uses' => 'ProfilesController@update',
+        'as' => 'profile.update',
+    ]);
+});
